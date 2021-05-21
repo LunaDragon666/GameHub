52629c7ad051481ac54fef9f48c93530cf93d453
LunaDragon666 <74667575+LunaDragon666@users.noreply.github.com>
2021-05-21 14:43:54 +0200

    Update cartList.js

--- js/constants/cartList.js
+++ js/constants/cartList.js
@@ -3,7 +3,7 @@ export const cartArray = [
       name: "Cyberpunk 2077",
       genre: "RPG",
       release: 2020,
-      image: "../img/gamefeature.jpg",
+      image: "./img/gamefeature.jpg",
       id: "1",
       price: "$57.95",
    }
