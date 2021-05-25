77062ded4adecbc1ead17021378b88a85e55c80d
LunaDragon666 <74667575+LunaDragon666@users.noreply.github.com>
2021-05-25 21:22:43 +0200

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,6 @@
-import { cartArray } from "./js/constants/cartList.js";
+/* import { cartArray } from "./js/constants/cartList.js";
 console.log(cartArray);
 
 cartArray.forEach(function(products) {
 
-});
+}); */
