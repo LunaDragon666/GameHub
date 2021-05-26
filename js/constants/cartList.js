b18023010dbb9f5b9d1902aaffbd75105445dc83
LunaDragon666 <74667575+LunaDragon666@users.noreply.github.com>
2021-05-27 00:02:05 +0200

    Update cartList.js

--- js/constants/cartList.js
+++ js/constants/cartList.js
@@ -1,10 +1,30 @@
 /* export const cartArray = [
    {
-      name: "Cyberpunk 2077",
-      genre: "RPG",
-      release: 2020,
+      name: "Cyberpunk",
+      title: "2077",
       image: "./img/gamefeature.jpg",
       id: "1",
+      price: "$57.95",
+   },
+   {
+      name: "Assassin's Creed",
+      title: "Valhalla",
+      image: ".img/gamefeature1.jpg",
+      id: "2",
+      price: "$57.95",
+   },
+   {
+      name: "Watch Dogs",
+      title: "Legion",
+      image: ".img/gamefeature2.jpg",
+      id: "3",
+      price: "$57.95",
+   },
+   {
+      name: "Mortal Kombat 11",
+      title: "Ultimate bundle",
+      image: ".img/gamefeature3.jpg",
+      id: "4",
       price: "$57.95",
    }
 ]; */ 
