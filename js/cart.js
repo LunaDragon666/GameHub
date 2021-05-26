afe4e44bfb3d2fa00ce9108eef5e2a68694e3b89
LunaDragon666 <74667575+LunaDragon666@users.noreply.github.com>
2021-05-27 00:02:02 +0200

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -10,7 +10,14 @@ cartArray.forEach(function(products) {
       <h2>${product.name}</h2>
       <div style="background-image: url(${product.image}) class="product-image")></div>
       <div class="product-price">${product.price}</div>
-      <button class="product-button">Add to cart</button>
+      <button class="product-button" data-product=${product.id}"">Add to cart</button>
    </div>
    `
-}); */ 
+}) 
+
+const buttons = document.querySelector(".buybtn");
+buttons.forEach(function(button){
+   button.onclick = (function(event){
+      console.log(event.target.dataset);
+   }
+})*/ 
