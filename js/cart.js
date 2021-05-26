f84d5b35aa0b2908b1d7b4e8124cd03dbe376311
LunaDragon666 <74667575+LunaDragon666@users.noreply.github.com>
2021-05-26 10:34:54 +0200

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,16 @@
 /* import { cartArray } from "./js/constants/cartList.js";
 console.log(cartArray);
 
-cartArray.forEach(function(products) {
+const productsContainer = document.querySelector(".products");
 
+cartArray.forEach(function(products) {
+   productsContainer.innerHTML +=
+   `
+   <div class="product">
+      <h2>${product.name}</h2>
+      <div style="background-image: url(${product.image}) class="product-image")></div>
+      <div class="product-price">${product.price}</div>
+      <button class="product-button">Add to cart</button>
+   </div>
+   `
 }); */ 
