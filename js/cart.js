0c0c8080fbfc145b0f938bc4867b5112ea76969d
LunaDragon666 <74667575+LunaDragon666@users.noreply.github.com>
2021-05-28 10:12:09 +0200

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -2,13 +2,17 @@
 console.log(cartArray);
 
 const productsContainer = document.querySelector(".products");
+const cart = document.querySelector(".cart");
+const cartList = document.querySelector(".cart-list");
+const totalContainer = document.querySelector(".total");
+let cartArray = [];
 
 cartArray.forEach(function(products) {
    productsContainer.innerHTML +=
    `
    <div class="product">
-      <h2>${product.name}</h2>
-      <div style="background-image: url(${product.image}) class="product-image")></div>
+      <h5>${product.name}</h5>
+      <div style="background-image: url(${cartElement.image}) class="product-image")></div>
       <div class="product-price">${product.price}</div>
       <button class="product-button" data-product=${product.id}"">Add to cart</button>
    </div>
@@ -18,6 +22,28 @@ cartArray.forEach(function(products) {
 const buttons = document.querySelector(".buybtn");
 buttons.forEach(function(button){
    button.onclick = (function(event){
-      console.log(event.target.dataset);
+      const itemToAdd = cartArray.find(item => item.id === event.target.dataset.product);
+      cartArray.push(itemToAdd);
+      showCart(cartArray);
+      localStorage.setItem("cartList", JSON.stringify(cartArray));
+
+      console.log(cartArray);
    }
-})*/ 
+})
+
+function showCart(cartItems) {
+   cart.style.display = "block";
+   cartList.innerHTML = "";
+   let total = 0;
+   cartItems.forEach(function(cartElement){
+      total += cartElement.price;
+      cartList.innerHTML +=
+      `
+      <div class="cart-item">
+         <h6>${cartElement.name}</h6>
+         <img src="${cartElement.image}" alt="${cartElement.name}" class="cart-image">
+      </div>
+      `
+   })
+totalContainer.innerHTML = `Total: ${total}`;
+}*/ 
