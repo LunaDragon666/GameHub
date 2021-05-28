c8d96ebbcdd0ddefefecee9b3ff33b25e0e9cd1d
LunaDragon666 <74667575+LunaDragon666@users.noreply.github.com>
2021-05-28 10:12:12 +0200

    Update cartList.js

--- js/constants/cartList.js
+++ js/constants/cartList.js
@@ -2,28 +2,28 @@
    {
       name: "Cyberpunk",
       title: "2077",
-      image: "./img/cyberpunk2077.jpg",
+      image: "./img/games/cyberpunk2077.jpg",
       id: "1",
       price: "$37.95",
    },
    {
       name: "Assassin's Creed",
       title: "Valhalla",
-      image: ".img/assassinscreed-valhalla.jpg",
+      image: ".img/games/assassinscreed-valhalla.jpg",
       id: "2",
       price: "$48.97",
    },
    {
       name: "Watch Dogs",
       title: "Legion",
-      image: ".img/watchdogs-legion.jpg",
+      image: ".img/games/watchdogs-legion.jpg",
       id: "3",
       price: "$27.64",
    },
    {
       name: "Mortal Kombat 11",
       title: "Ultimate bundle",
-      image: ".img/mortalkombat11.jpg",
+      image: ".img/games/mortalkombat11.jpg",
       id: "4",
       price: "$12.45",
    }
