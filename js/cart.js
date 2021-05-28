b03027363c5054de9183bfda2840b367c3a89b41
LunaDragon666 <74667575+LunaDragon666@users.noreply.github.com>
2021-05-28 19:10:23 +0200

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -38,53 +38,13 @@ function showCart() {
         cartList.innerHTML += `
             <div class="cart-item">
              <h6>${cartElement.name}</h6>
+             <p class="cart-detail">${cartElement.title}</p>
              <img src="${cartElement.image}" alt="${cartElement.name}" class="cart-image">
+             <p class="cart-price">$${cartElement.price}</p>
             </div>
         `;
     });
 
-    totalContainer.innerHTML = `Total: ${total}`;
+    totalContainer.innerHTML = `Total: <span class="p">$ ${total}</span>`;
     cart.style.display = "flex";
 }
-
-
-// import { cartArray } from "./constants/cartList.js";
-// console.log(cartArray);
-
-// // const productsContainer = document.querySelector(".products");
-// const cart = document.querySelector(".cart");
-// const cartList = document.querySelector(".cart-list");
-// const totalContainer = document.querySelector(".total");
-
-// const buttons = document.querySelectorAll(".buybtn");
-
-// buttons.forEach(function(button) {
-//     button.onclick = (function (event) {
-//         const id = event.target.dataset.id;
-//         const itemToAdd = cartArray.find(item => item.id === id);
-
-//         cartArray.push(itemToAdd);
-//         showCart(cartArray);
-//         localStorage.setItem("cartList", JSON.stringify(cartArray));
-
-//         console.log(cartArray);
-//     });
-// });
-
-// function showCart(cartItems) {
-//    cart.style.display = "flex";
-//    cartList.innerHTML = "";
-//    let total = 0;
-//    cartItems.forEach(function(cartElement){
-//       total += cartElement.price;
-//       cartList.innerHTML +=
-//       `
-//       <div class="cart-item">
-//          <h6>${cartElement.name}</h6>
-//          <p class="cart-detail">${cartElement.title}</p>
-//          <img src="${cartElement.image}" alt="${cartElement.name}" class="cart-image">
-//       </div>
-//       `
-//    })
-
-// };
