2cfa9682e420369758fb5ecc75745c02f840ec09
LunaDragon666 <74667575+LunaDragon666@users.noreply.github.com>
2021-05-28 11:52:50 +0200

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,34 +1,34 @@
-import { cartArray } from "./js/constants/cartList.js";
+import { cartArray } from "./constants/cartList.js";
 console.log(cartArray);
 
 const productsContainer = document.querySelector(".products");
 const cart = document.querySelector(".cart");
 const cartList = document.querySelector(".cart-list");
 const totalContainer = document.querySelector(".total");
-let cartArray = [];
 
-cartArray.forEach(function(products) {
+cartArray.forEach(function(product) {
    productsContainer.innerHTML +=
    `
    <div class="product">
       <h5>${product.name}</h5>
-      <div style="background-image: url(${cartElement.image}) class="product-image")></div>
+      <div style="background-image: url(${product.image})" class="product-image"></div>
       <div class="product-price">${product.price}</div>
       <button class="product-button" data-product=${product.id}"">Add to cart</button>
    </div>
    `
-}) 
+});
 
-const buttons = document.querySelector(".buybtn");
-buttons.forEach(function(button){
-   button.onclick = (function(event){
-      const itemToAdd = cartArray.find(item => item.id === event.target.dataset.product);
-      cartArray.push(itemToAdd);
-      showCart(cartArray);
-      localStorage.setItem("cartList", JSON.stringify(cartArray));
+const buttons = document.querySelectorAll(".buybtn");
+buttons.forEach(function(button) {
+    button.onclick = (function (event) {
+        console.log(event.target.dataset);
+        const itemToAdd = cartArray.find(item => item.id === event.target.dataset.product);
+        cartArray.push(itemToAdd);
+        showCart(cartArray);
+        localStorage.setItem("cartList", JSON.stringify(cartArray));
 
-      console.log(cartArray);
-   }
+        console.log(cartArray);
+    });
 });
 
 function showCart(cartItems) {
